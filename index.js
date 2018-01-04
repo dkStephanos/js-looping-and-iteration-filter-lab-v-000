01c3b931e4463a623fb545aa63d174635c95ea4e
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 18:06:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  const collection = [];
+
+  for(const driver of drivers) {
+    if (driver == string) {
+      collection.push(driver)
+    }
+  }
+  return collection
+}
